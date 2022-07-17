ce1513e8c26aa1879aeb13d3c857d5c871365701
Chhay Toch <chhaytoch.dev@gmail.com>
2022-07-17 15:50:11 +0700

    fix: implement multilang

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -16,6 +16,7 @@ export const translationsJson = {
 
 // Create the 'translations' object to provide full intellisense support for the static json files.
 convertLanguageJsonToObject(km);
+convertLanguageJsonToObject(en);
 
 export const i18n = i18next
   // pass the i18n instance to react-i18next.
@@ -26,11 +27,10 @@ export const i18n = i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources: translationsJson,
-    lng: 'km',
+    lng: localStorage.getItem('lang') || 'km',
     fallbackLng: 'km',
     debug:
       process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test',
-
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
