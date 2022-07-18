45e921ba0d71b88445e93759b4f1ad7e006c0340
Chhay Toch <chhaytoch.dev@gmail.com>
2022-07-18 22:05:20 +0700

    build: improve prettier, tsconfig, eslint, and upgrade package version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const prettierOptions = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
-);
+const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
 
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: ['react-app', 'prettier', 'prettier/react'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', prettierOptions],
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx', '.js', '.jsx'] }],
+    'no-unused-vars': 'off',
+    'no-unused-variable': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-variable': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
   overrides: [
     {
