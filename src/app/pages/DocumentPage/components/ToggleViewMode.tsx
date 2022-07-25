35668a8e5a8f659e94bede808cdc77864aedf8d7
Chhay Toch <chhaytoch.dev@gmail.com>
2022-07-25 12:00:33 +0700

    fix: replace Radio button with Segment component

--- src/app/pages/DocumentPage/components/ToggleViewMode.tsx
+++ src/app/pages/DocumentPage/components/ToggleViewMode.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Radio } from 'antd';
+import { Segmented } from 'antd';
 import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons';
 
 interface ToggleViewModeProps {
   mode: string;
-  onSwitchViewMode: (e) => void;
+  onSwitchViewMode: (value) => void;
 }
 
 const ToggleViewMode = ({ mode, onSwitchViewMode }: ToggleViewModeProps) => {
   return (
-    <Radio.Group value={mode} onChange={onSwitchViewMode}>
-      <Radio.Button value="card">
-        <AppstoreOutlined />
-      </Radio.Button>
-      <Radio.Button value="table">
-        <BarsOutlined />
-      </Radio.Button>
-    </Radio.Group>
+    <Segmented
+      value={mode}
+      options={[
+        {
+          value: 'table',
+          icon: <BarsOutlined />,
+        },
+        {
+          value: 'card',
+          icon: <AppstoreOutlined />,
+        },
+      ]}
+      onChange={onSwitchViewMode}
+    />
   );
 };
 
