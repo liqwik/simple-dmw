5ec85b706ec1f6ccbca431d9cf5dd29eab92acb9
Chhay Toch <chhaytoch.dev@gmail.com>
2022-10-02 13:29:35 +0700

    fix: update ReportService

--- src/services/ReportService.ts
+++ src/services/ReportService.ts
@@ -9,9 +9,42 @@ class ReportService {
   }
 
   async getTotalDocByStatus(startDate, endDate) {
-    return this.http.get(`${ApiRoute.report()}/docs/status`, {
+    const data = await this.http.get(`${ApiRoute.report()}/docs/status`, {
       params: { start: startDate, end: endDate },
     });
+
+    const result = data.reduce(
+      (prev, cur) => {
+        const { _id, totalDoc } = cur;
+        prev.totalDoc += totalDoc;
+
+        if (_id.docStatus === 'normal') {
+          prev.totalNormal += cur.totalDoc;
+        }
+        if (_id.docStatus === 'urgent') {
+          prev.totalUrgent += cur.totalDoc;
+        }
+
+        if (_id.sign) {
+          prev.totalSigned += cur.totalDoc;
+        }
+
+        if (!_id.sign) {
+          prev.totalUnsign += cur.totalDoc;
+        }
+
+        return prev;
+      },
+      {
+        totalDoc: 0,
+        totalNormal: 0,
+        totalUrgent: 0,
+        totalSigned: 0,
+        totalUnsign: 0,
+      },
+    );
+
+    return result;
   }
 
   async getTotalDocByDocType(startDate, endDate) {
