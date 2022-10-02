838c1bde6487918ec538a2568dd707f8675212db
Chhay Toch <chhaytoch.dev@gmail.com>
2022-10-02 13:29:10 +0700

    fix: display user profile name in Header

--- src/app/components/UI/GlobalHeader/HeaderAvatar.tsx
+++ src/app/components/UI/GlobalHeader/HeaderAvatar.tsx
@@ -1,19 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu, Dropdown, Avatar } from 'antd';
 import { AppStorage } from 'utils';
 import { useTranslation } from 'react-i18next';
+import { useLoginSlice } from 'app/pages/AuthPage/slice';
+import { useSelector } from 'react-redux';
+import { selectLogin } from 'app/pages/AuthPage/slice/selectors';
 
 export const HeaderAvatar = () => {
+  useLoginSlice();
+  const { user } = useSelector(selectLogin);
   const { t } = useTranslation();
-  const [displayName, setDisplayName] = useState('Anonymous');
 
-  useEffect(() => {
-    const userData = AppStorage.getAuthData();
-    if (userData && userData.email) {
-      setDisplayName(userData.email.split('@')[0]);
+  const displayName = () => {
+    let result = '';
+
+    if (user.fn) {
+      result = user.fn + ' ';
     }
-  }, []);
+
+    if (user.ln) {
+      result += user.ln;
+    }
+
+    return result;
+  };
 
   const handleLogout = () => {
     AppStorage.removeAuthData(true);
@@ -27,7 +38,7 @@ export const HeaderAvatar = () => {
           items={[
             {
               key: 'name',
-              label: displayName,
+              label: displayName(),
             },
             {
               key: 'center',
